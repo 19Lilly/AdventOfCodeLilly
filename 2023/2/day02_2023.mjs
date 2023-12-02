15c313839eda500d9e19ac90e2f306022a0bc476
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2023-12-02 14:05:37 +0100

    some refactor of code

--- 2023/2/day02_2023.mjs
+++ 2023/2/day02_2023.mjs
@@ -5,18 +5,20 @@ const lines = readFileSync("day02_2023.txt", { encoding: "utf-8" }) // read day?
   .trim() // Remove starting/ending whitespace
   .split("\n"); // Split on newline
 
+const result = lines
+  .map((line) => line.split(/(?:,|;|:)+/))
+  .map((arr) => arr.flatMap((item) => item.trim()))
+  .map((item) => item.slice(1))
+  .map((item) => item.map((item) => item.split(" ")))
+  .map((item) =>
+    item.map((item) => {
+      return [+item[0], item[1]];
+    })
+  );
+
 function part1() {
   let indexes = [];
-  const res = lines
-    .map((line) => line.split(/(?:,|;|:)+/))
-    .map((arr) => arr.flatMap((item) => item.trim()))
-    .map((item) => item.slice(1))
-    .map((item) => item.map((item) => item.split(" ")))
-    .map((item) =>
-      item.map((item) => {
-        return [+item[0], item[1]];
-      })
-    )
+  result
     .map((item) => {
       item.map((item) => {
         item[0] > 12 && item[1] === "red"
@@ -37,34 +39,26 @@ function part1() {
 }
 
 function part2() {
-  const res1 = lines
-    .map((line) => line.split(/(?:,|;|:)+/))
-    .map((arr) => arr.flatMap((item) => item.trim()))
-    .map((item) => item.slice(1))
-    .map((item) => item.map((item) => item.split(" ")))
-    .map((item) =>
-      item.map((item) => {
-        return [+item[0], item[1]];
-      })
-    )
+  const result2 = result
     .map((arr) => {
-      let res = new Array(3);
-      res[0] = arr
+      let res1 = new Array(3);
+      res1[0] = arr
         .filter((item) => item[1] === "red")
         .map((item) => item[0])
         .sort((a, b) => b - a)[0];
-      res[1] = arr
+      res1[1] = arr
         .filter((item) => item[1] === "green")
         .map((item) => item[0])
         .sort((a, b) => b - a)[0];
-      res[2] = arr
+      res1[2] = arr
         .filter((item) => item[1] === "blue")
         .map((item) => item[0])
         .sort((a, b) => b - a)[0];
-      return res.reduce((acc, curr) => acc * curr);
-    });
+      return res1.reduce((acc, curr) => acc * curr);
+    })
+    .reduce((acc, curr) => acc + curr, 0);
 
-  console.log(res1.reduce((acc, curr) => acc + curr, 0));
+  console.log(result2);
 }
 
 part1();
