1ecaba3cb839d0d05a519fb2a7a9787ccdc7ca7d
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2023-12-05 00:21:16 +0100

    solution part 2

--- 2023/4/day04_2023.mjs
+++ 2023/4/day04_2023.mjs
@@ -6,25 +6,43 @@ const input = readFileSync("day04_2023.txt", { encoding: "utf-8" }) // read day?
   .split("\n")
   .map((item) => item.split(/(?::|\|)+/))
   .map((item) => item.slice(1))
-  .map((item) => item.map((item) => item.match(/\d+/g))); // Split on newline
-//.map(Number(item)) // convert string to number
+  .map((item) => item.map((item) => item.match(/\d+/g)));
 
 function part1() {
   const res = input
     .map((item) => item[1].filter((x) => item[0].includes(x)))
     .map((item) => (item.length === 0 ? 0 : 2 ** (item.length - 1)))
     .reduce((acc, curr) => acc + curr, 0);
-  console.log(res);
+  // console.log(res);
 }
 
 function part2() {
-  const numberOfRepetition = input
-    .map((item) => item[1].filter((x) => item[0].includes(x)))
-    .map((item) => item.length);
-  console.log(numberOfRepetition);
-}
+  const matchingList = input.map((card) => {
+    let matching = 0;
+    card[1].forEach((number) => {
+      if (card[0].includes(number)) {
+        matching++;
+      }
+    });
+    return matching;
+  });
 
+  const countSelfAndSpawnedCards = (matchArr, index) => {
+    let cards = 1;
+    for (let i = 0; i < matchArr[index]; i++) {
+      cards += countSelfAndSpawnedCards(matchArr, index + 1 + i);
+    }
+    return cards;
+  };
+
+  const sum = matchingList.reduce(
+    (sum, _, i, arr) => sum + countSelfAndSpawnedCards(arr, i),
+    0
+  );
+  console.log(sum);
+}
 part1();
 part2();
 
 //template using from https://github.com/tpatel/advent-of-code-2022/blob/main/template.mjs
+//part2 solution: https://github.com/TunHuang/advent-of-code-2023/blob/main/day-04/index.js
