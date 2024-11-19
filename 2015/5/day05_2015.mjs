696a549224110704b97a1dc4cca9b2858bfa0afe
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2024-11-19 22:36:43 +0100

    day 05 of 2015 solutions

--- 2015/5/day05_2015.mjs
+++ 2015/5/day05_2015.mjs
@@ -1,24 +1,33 @@
 import { readFileSync } from 'node:fs';
 
-const input = readFileSync('day05_2015_testData.txt', { encoding: 'utf-8' }) // read day??.txt content
+const input = readFileSync('day05_2015.txt', { encoding: 'utf-8' }) // read day??.txt content
   .replace(/\r/g, '') // remove all \r characters to avoid issues on Windows
   .trim() // Remove starting/ending whitespace
   .split('\n'); // Split on newline
 //.map(Number(item)) // convert string to number
-console.log(input);
+// console.log(input);
 function part1() {
   const findNiceStrings = array => {
-    {
-      {
-        {
-        }
-      }
-    }
+    const firstCondition = /ab|cd|pq|xy/;
+    const secondCondition = /(.*[aeiou].*){3,}/i;
+    const thirdCondition = /([a-zA-Z])\1/;
+    return array
+      .filter(item => !firstCondition.test(item))
+      .filter(item => secondCondition.test(item))
+      .filter(item => thirdCondition.test(item));
   };
+  console.log(findNiceStrings(input).length);
 }
 
 function part2() {
-  //do something here
+  const findNiceStrings = array => {
+    const firstCondition = /(..).*?\1/;
+    const secondCondition = /([a-zA-Z]).\1/;
+    return array
+      .filter(item => firstCondition.test(item))
+      .filter(item => secondCondition.test(item));
+  };
+  console.log(findNiceStrings(input).length);
 }
 
 part1();
